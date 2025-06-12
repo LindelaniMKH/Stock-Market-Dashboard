b72769182e6498042b6b71f68a077900b98336b4
LindelaniMKH <dlaminlindelani.15@gmail.com>
2025-06-12 23:25:25 +0200

    Switched to CDN and deleted all the files from the npm install

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+const { Chart } =  import('chart.js');
+
+
 function fetchData(){
     const apiKey = "J3MLMOFOUP0RAV8S";
     const link = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${apiKey}&datatype=json`;
