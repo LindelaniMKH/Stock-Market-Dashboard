59aa0c77130232be2663764b4f314ecd918d4eba
LindelaniMKH <dlaminlindelani.15@gmail.com>
2025-06-23 00:18:31 +0200

    I made it possible to display the date of the firstl element

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 
-const { Chart } =  import('chart.js');
+//const ctx = document.getElementById("Chart").getContext('2d');
 
 
 function fetchData(){
     const apiKey = "J3MLMOFOUP0RAV8S";
-    const link = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${apiKey}&datatype=json`;
+    const link = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${apiKey}&datatype=json&outputsize=compact`;
 
     fetch(link)
         .then(response => {
-            console.log(response.json());
+            if(!response.ok){
+                throw new Error ("HTTPS error, unable to fetch data");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const meta  = data["Meta Data"];
+            const timeSeries = data["Time Series (Daily)"];
+            const timeSeries_arr = Object.keys(timeSeries); //returns an array from containing the various daye from the JSO. 
+            console.log(meta["2. Symbol"]);
+            console.log(timeSeries_arr[0]);
         });
 }
 
